325c02b1f8ed5addc6b805ceeaa6129230d20580
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-03 19:57:56 +0900

    Fix | index.ts : 단순하게 Hello, world 내보내기로 변경

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,7 +3,11 @@ import path from "path";
 
 export function app() {
   const app = express();
+  // app.use(express.static(path.join(__dirname, 'public')));
 
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.get("/", (req, res) => {
+    res.send("Hello, world");
+  })
+
   return app;
 }
