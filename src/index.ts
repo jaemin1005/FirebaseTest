2f496ce277644ead632cc13274d1353f731f8ff2
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-03 21:41:18 +0900

    Fix | index.ts : listen 추가

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import path from "path";
 
-export function app() {
-  const app = express();
-  // app.use(express.static(path.join(__dirname, 'public')));
 
-  app.get("/", (req, res) => {
-    res.send("Hello, world");
-  })
+const app = express();
+// app.use(express.static(path.join(__dirname, 'public')));
 
-  return app;
-}
+app.get("/", (req, res) => {
+  res.send("Hello, world");
+})
+
+app.listen(3000);
