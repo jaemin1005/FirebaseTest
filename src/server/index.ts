588411e28c0891aa27e815fbae519925a09a74e3
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-03 16:35:59 +0900

    New | index.ts : 서버 구동되는 파일 생성
    단순히 '/' 경로 요청시 index.html을 넘겨준다

---
+++ src/server/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
